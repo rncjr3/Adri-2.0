53b447ef4b407d72d0a3586a826b916cd0146925
Rafael <rncjr3@yahoo.com>
2023-02-20 14:25:10 -0600

    Robot should work with Xbox Controller

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,32 @@
+package frc.robot;
+
+public class Constants {
+    // Get this distance in meters, convert it.
+    public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.2794;/// distance from left wheels to right wheels
+    public static final double DRIVETRAIN_WHEELBASE_METERS = 0.2794;// distance from front motors to back motors
+
+    public static final int DRIVETRAIN_PIGEON_ID = 0;// don't need this, but if we get one, we need to assign it an ID
+
+    //Change the numbers to match the ID's, for the drive, steer, and encoder
+    // Set the Encoder ID in the Phoenix Tuner software
+    // Mess with the offset values so that the robot wheels end up straight, they are degree values
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 3;
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 4;
+    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 2;
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0); 
+
+    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 8;
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 7;
+    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 4;
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0);
+
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 1;
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 2;
+    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 3;
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0);
+
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 6;
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 5;
+    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 1;
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0);
+}
